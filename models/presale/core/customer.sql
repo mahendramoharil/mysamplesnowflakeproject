95c0c5cbb9903f1ded9e8eb74647f89366a79a35
m.m.mahendra@gmail.com <m.m.mahendra@gmail.com>
2024-07-03 12:50:20 +0000

    2nd commit

---
+++ models/presale/core/customer.sql
@@ -0,0 +1,5 @@
+select
+  id,
+  first_name,
+  last_name
+from HEVOSAMPLEDB.public.pgsql_raw_customers
---
+++ models/presale/core/payments.sql
@@ -0,0 +1,13 @@
+with customer_payments as (
+  select
+    o.user_id,
+    sum(p.payment_amount) as customer_lifetime_value
+  from HEVOSAMPLEDB.public.pgsql_raw_payments p
+  join HEVOSAMPLEDB.public.pgsql_raw_orders o
+    on p.order_id = o.id
+  group by o.user_id
+)
+select
+  user_id,
+  customer_lifetime_value
+from customer_payments
