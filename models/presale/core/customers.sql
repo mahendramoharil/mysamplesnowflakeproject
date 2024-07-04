3a9b4f38470c4642fef305d87946f701c2c80bd4
mahendra moharil <m.m.mahendra@gmail.com>
2024-07-04 12:35:00 +0000

    1

---
+++ models/presale/core/customers.sql
@@ -0,0 +1,5 @@
+select
+  id,
+  first_name,
+  last_name
+from HAVOSAMPLEDB.public.pgsql_raw_customers
---
+++ models/presale/core/orders.sql
@@ -0,0 +1,16 @@
+-- models/marts/core/orders.sql
+with customer_orders as (
+  select
+    user_id,
+    min(order_date) as first_order,
+    max(order_date) as most_recent_order,
+    count(id) as number_of_orders
+  from HAVOSAMPLEDB.public.pgsql_raw_orders
+  group by user_id
+)
+select
+  user_id,
+  first_order,
+  most_recent_order,
+  number_of_orders
+from customer_orders
